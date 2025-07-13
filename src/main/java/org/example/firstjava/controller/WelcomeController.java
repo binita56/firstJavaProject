d7cef1f98fb8f1190aec114257542612a9b06571
binita shrestha <stha.binita56@gmail.com>
2025-07-13 14:05:43 +0545

    Initial commit

---
+++ src/main/java/org/example/firstjava/controller/WelcomeController.java
@@ -0,0 +1,4 @@
+package org.example.firstjava.controller;
+
+public class WelcomeController {
+}
---
+++ src/test/java/org/example/firstjava/FirstJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firstjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
